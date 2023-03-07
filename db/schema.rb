84124804b360814eb2df8f0327f8986dc880ccc9
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-03-07 12:18:41 +0100

    pundit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_161338) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_110139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -91,6 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_161338) do
     t.integer "phone_number"
     t.boolean "is_vet", default: false
     t.string "address"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
