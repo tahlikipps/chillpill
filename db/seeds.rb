8a4583139ac239a8e2f318141890dac61f12bfd3
Angie Xu <anyixu98@gmail.com>
2023-03-08 15:54:30 +0100

    Added meds controller and forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+PetCarer.destroy_all
+Pet.destroy_all
+User.destroy_all
+
+leia = User.create!(first_name: "Leia", last_name:"Gracia", password:123456, address:"Carrer del Bruc 149 Barcelaon", phone_number:123456, email:"leia1@gmail.com")
+blair = Pet.create!(name: "Blair", birth_date: "01/03/2022", address: "Carrer bruc", species: "dog")
+
+PetCarer.create!(status: 1, is_owner: true, pet: blair, user: leia)
