403290de00a2cda226ca8e426c90fa09406f64ac
Tahli Kipps <tahli.kipps@gmail.com>
2023-03-09 11:54:30 +0100

    changes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,6 +2,7 @@ class PetsController < ApplicationController
   def show
     @medications = Medication.all.where(user_id: current_user.id)
     @pet_carers = PetCarer.all.where(user_id: current_user.id)
+    @owner = PetCarer.all.where(user_id: current_user.id, is_owner: true)
     @pet = Pet.find(params[:id])
 
     authorize @pet
