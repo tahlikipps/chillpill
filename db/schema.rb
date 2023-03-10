7033a3da2484a0b4dcd3a04f21ae1b2887b7b5a1
Tahli Kipps <tahli.kipps@gmail.com>
2023-03-10 10:48:37 +0100

    removing pillbox code

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_111243) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_151820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
