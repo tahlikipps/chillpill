8ebae71e3c6364d169f57a218ad4063346bc5dbc
Tahli Kipps <tahli.kipps@gmail.com>
2023-03-16 14:25:09 +0100

    seeds and filtering add pet card on show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,7 @@ puts "Destroying past seeds..."
 
 Message.destroy_all
 Chat.destroy_all
+MedicationAdministration.destroy_all
 PetCarer.destroy_all
 Medication.destroy_all
 Pet.destroy_all
@@ -109,17 +110,142 @@ user5 = User.new(
   first_name: "Tahli",
   last_name: "Kipps",
   phone_number: "+344461327",
-  address: "Madrid",
-  email: "tahli@gmail.com",
+  address: "Carrer del Bruc, 149, Barcelona",
+  email: "tahli.kipps@gmail.com",
   password: "123456",
-  is_vet: true
+  is_vet: false,
+  created_at: DateTime.now-7.days,
+  updated_at: DateTime.now-7.days
 )
-file = URI.open("https://images.unsplash.com/photo-1635416651032-191ab4bf66a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2942&q=80")
-user5.photo.attach(io: file, filename: "girlly.jpg", content_type: "image/jpeg")
+file = URI.open("https://avatars.githubusercontent.com/u/122308794?v=4")
+user5.photo.attach(io: file, filename: "Tahli.png", content_type: "image/png")
 
 user5.save!
 
+pet5 = Pet.new(
+  name: "Blair",
+  species: 'Dog',
+  address: "Carrer de Bruc 149, Barcelona",
+  birth_date: Date.new(2022, 2, 28),
+  created_at: DateTime.now-7.days,
+  updated_at: DateTime.now-7.days
+)
+pet5.save!
+
+file = File.open("app/assets/images/Blair.png")
+pet5.photo.attach(io: file, filename: "blair.png", content_type: "image/png")
+pet5.save!
+
+medication = Medication.new(
+  name: "Conofite",
+  medication_type: "Liquid",
+  dosage: "3 drops in & outside ear",
+  time_period: "Daily",
+  frequency: 2,
+  start_date: Date.new(2023, 3, 9),
+  end_date: Date.new(2022, 3, 20),
+  created_at: DateTime.now-7.days
+
+)
+medication.pet = pet5
+medication.save!
+
+PetCarer.create!(
+  status: 1,
+  pet: pet5,
+  user: user5,
+  is_owner: true
+)
+
 user6 = User.new(
+  first_name: "Ryan",
+  last_name: "Ebert",
+  phone_number: "+344461327",
+  address: "Carrer del Bruc, 149, Barcelona",
+  email: "ryan@gmail.com",
+  password: "123456",
+  is_vet: false,
+  created_at: DateTime.now-7.days,
+  updated_at: DateTime.now-7.days
+)
+file = URI.open('https://media.licdn.com/dms/image/D4E03AQG8jNnnT-cVHQ/profile-displayphoto-shrink_400_400/0/1668670394925?e=1684368000&v=beta&t=P1t8g-DlyyiS7nnh6up9cN5kAzRZPIr3pQV79W-BLbo')
+user6.photo.attach(io: file, filename: "Ryan.png", content_type: "image/png")
+
+user6.save!
+
+PetCarer.create!(
+  status: 1,
+  pet: pet5,
+  user: user6,
+  is_owner: false
+)
+
+PetCarer.create!(
+  status: 1,
+  pet: pet5,
+  user: user2,
+  is_owner: false
+)
+
+
+medicationadministration1 = MedicationAdministration.new(
+  date: Date.new(2023, 3, 12),
+  medication_id: medication.id,
+  is_given: true,
+  user_id: user6.id
+)
+medicationadministration1.save!
+
+
+medicationadministration2 = MedicationAdministration.new(
+  date: Date.new(2023, 3, 13),
+  medication_id: medication.id,
+  is_given: true,
+  user_id: user5.id
+)
+medicationadministration2.save!
+
+medicationadministration3 = MedicationAdministration.new(
+  date: Date.new(2023, 3, 14),
+  medication_id: medication.id,
+  is_given: true,
+  user_id: user5.id
+)
+medicationadministration3.save!
+
+medicationadministration4 = MedicationAdministration.new(
+  date: Date.new(2023, 3, 15),
+  medication_id: medication.id,
+  is_given: true,
+  user_id: user5.id
+)
+medicationadministration4.save!
+
+medicationadministration5 = MedicationAdministration.new(
+  date: Date.new(2023, 3, 16),
+  medication_id: medication.id,
+)
+medicationadministration5.save!
+
+medicationadministration6 = MedicationAdministration.new(
+  date: Date.new(2023, 3, 16),
+  medication_id: medication.id,
+)
+medicationadministration6.save!
+
+medicationadministration7 = MedicationAdministration.new(
+  date: Date.new(2023, 3, 17),
+  medication_id: medication.id,
+)
+medicationadministration7.save!
+
+medicationadministration8 = MedicationAdministration.new(
+  date: Date.new(2023, 3, 18),
+  medication_id: medication.id,
+)
+medicationadministration8.save!
+
+user8 = User.new(
   first_name: "Marta",
   last_name: "Garcia",
   phone_number: "+344361327",
@@ -129,9 +255,22 @@ user6 = User.new(
   is_vet: true
 )
 file = URI.open("https://images.unsplash.com/photo-1609138271629-571665f418a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")
-user6.photo.attach(io: file, filename: "new.jpg", content_type: "image/jpeg")
+user8.photo.attach(io: file, filename: "new.jpg", content_type: "image/jpeg")
 
-user6.save!
+user8.save!
+
+user9 = User.new(
+  first_name: "Claire",
+  phone_number: "+344361327",
+  address: "Barcelona",
+  email: "claire@rover.com",
+  password: "123456",
+  is_vet: false
+)
+file = URI.open("https://images.unsplash.com/photo-1609138271629-571665f418a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80")
+user9.photo.attach(io: file, filename: "new.jpg", content_type: "image/jpeg")
+
+user9.save!
 
 # pet = Pet.new(
 #   name: "Catty",
